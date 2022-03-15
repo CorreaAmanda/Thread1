4a15b7d1b9cd73d43be686626555c1dcb6d98fdb
CorreaAmanda <101160744+CorreaAmanda@users.noreply.github.com>
2022-03-15 00:38:02 -0300

    Update ThreadId.java

--- src/controlador/ThreadId.java
+++ src/controlador/ThreadId.java
@@ -6,7 +6,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		for (int idThread = 0 ; idThread < 3 ; idThread++) {
+		for (int idThread = 0 ; idThread < 5 ; idThread++) {
 			
 			Thread threadId = new ThreadId(idThread);
 			threadId.start();
